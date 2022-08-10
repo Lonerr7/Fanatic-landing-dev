1b50c9ba5db1998b86a60c24cd1594207c40cf4e
nic3guy <krabik2342@gmail.com>
2022-08-10 12:42:22 +0300

    6. Remade price star into css

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,14 @@
 import * as functions from './modules/functions.js';
+import $ from 'jquery';
 
 // Checking if browsers supports .webp
 functions.isWebp();
 
+$(function () {
+  //* Popup
+
+  $('.specs__btn').on('click', (e) => {
+    e.preventDefault();
+    console.log(`clicked`);
+  });
+});
